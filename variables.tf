bb7727888e8bd4477cfd67aa61fff9ee874214c9
Anna Shipman <anna@annashipman.co.uk>
2015-03-31 11:30:18 +0100

    Change variable names for alphabetical order
    When running `terraform`, variables are prompted for on the command line. The AWS secret key and access key need to be entered, but for the rest, for now, the default can be accepted. This change puts the secret key and access key up first so you can just enter them and then press return through the rest of them, for ease of use.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "access_key" { 
+variable "aws_access_key" { 
   description = "AWS access key"
 }
 
-variable "secret_key" { 
+variable "aws_secret_key" { 
   description = "AWS secert access key"
 }
 
