d7ee6f2b4e6138d2391f75b7e2f314a6af1c62b9
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-01 17:12:30 +0100

    Add wildcard DNS record for applications
    By adding this record, we don't need to worry about creating each application's DNS record manually.
    Instead, all the subdomains will be resolved to the main router DNS name.

--- dns-records.tf
+++ dns-records.tf
@@ -7,6 +7,15 @@ resource "aws_route53_record" "router" {
   records = ["${aws_elb.router.dns_name}"]
 }
 
+/* Wildcard CNAME record */
+resource "aws_route53_record" "wildcard" {
+  zone_id = "${aws_route53_zone.public.zone_id}"
+  name = "*.hipache.tsuru.paas.alphagov.co.uk"
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_route53_record.router.name}"]
+}
+
 /* Public Hosted Zone */
 resource "aws_route53_zone" "public" {
    name = "tsuru.paas.alphagov.co.uk"
