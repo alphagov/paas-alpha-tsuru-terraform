f8b8c82862973a1ae6af092d39d3dd2395773eb2
Jim Conner <jconner@equalexperts.com>
2015-04-10 11:05:17 +0100

    Changed AMI and nework for postgres machine

--- postgres-server.tf
+++ postgres-server.tf
@@ -1,16 +1,11 @@
 /* Single instance postgres server */
 resource "aws_instance" "postgres" {
-  ami = "${lookup(var.nat_ami, var.region)}"
+  ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.public.id}"
+  subnet_id = "${aws_subnet.public1.id}"
   security_groups = ["${aws_security_group.default.id}", "${aws_security_group.postgres.id}"]
   key_name = "${aws_key_pair.deployer.key_name}"
-  source_dest_check = false
   tags = {
     Name = "tsuru-postgres"
-  }
-  connection {
-    user = "ec2-user"
-    key_file = "ssh/insecure-deployer"
   }
 }
