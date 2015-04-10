08bcdb335dd804f5fc82e47d8b2898d63b6688b2
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-10 14:35:10 +0100

    Add CNAME wildcard DNS record
    This record allows us to reach any application deployed to Tsuru without having to create a record for each application.

--- dns-records.tf
+++ dns-records.tf
@@ -7,6 +7,15 @@ resource "aws_route53_record" "router" {
   records = ["${aws_elb.router.dns_name}"]
 }
 
+/* Application router wildcard record */
+resource "aws_route53_record" "wildcard" {
+  zone_id = "ZAO40KKT7J2PB"
+  name = "*.hipache.tsuru.paas.alphagov.co.uk"
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_route53_record.router.name}"]
+}
+
 /* Gandalf A record */
 resource "aws_route53_record" "gandalf" {
   zone_id = "ZAO40KKT7J2PB"
