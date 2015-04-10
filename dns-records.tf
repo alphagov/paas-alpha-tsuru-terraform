5cec04bb4532eee698c4536741f0cd7fe1dd9758
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-10 14:36:02 +0100

    Add a few DNS records for ease of use

--- dns-records.tf
+++ dns-records.tf
@@ -16,6 +16,24 @@ resource "aws_route53_record" "wildcard" {
   records = ["${aws_route53_record.router.name}"]
 }
 
+/* API external CNAME record */
+resource "aws_route53_record" "api-external" {
+  zone_id = "ZAO40KKT7J2PB"
+  name = "api.tsuru.paas.alphagov.co.uk"
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_elb.api-ext.dns_name}"]
+}
+
+/* API internal CNAME record */
+resource "aws_route53_record" "api-internal" {
+  zone_id = "ZAO40KKT7J2PB"
+  name = "internal.api.tsuru.paas.alphagov.co.uk"
+  type = "CNAME"
+  ttl = "60"
+  records = ["${aws_elb.api-int.dns_name}"]
+}
+
 /* Gandalf A record */
 resource "aws_route53_record" "gandalf" {
   zone_id = "ZAO40KKT7J2PB"
@@ -23,4 +41,13 @@ resource "aws_route53_record" "gandalf" {
   type = "A"
   ttl = "60"
   records = ["${aws_instance.gandalf.public_ip}"]
+}
+
+/* NAT A record */
+resource "aws_route53_record" "nat" {
+  zone_id = "ZAO40KKT7J2PB"
+  name = "nat.tsuru.paas.alphagov.co.uk"
+  type = "A"
+  ttl = "60"
+  records = ["${aws_instance.nat.public_ip}"]
 }
