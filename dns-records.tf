8156065f70662a6b70006e2c1b9d5f3d8a5fd196
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-10 14:24:07 +0100

    USe a pre-existing Hosted Zone
    Istead of creating a new hosted zone every time with Terraform, we're hardcoding a pre-exisisting Hosted Zone ID.
    This has been already delegated in Dyn so it's actually the only working Hosted Zone.
    Any new Hosted Zone created with Terraform would require some extra delegation steps in Dyn.

--- dns-records.tf
+++ dns-records.tf
@@ -1,13 +1,9 @@
 /* Router CNAME record */
 resource "aws_route53_record" "router" {
-  zone_id = "${aws_route53_zone.public.zone_id}"
+  zone_id = "ZAO40KKT7J2PB"
   name = "hipache.tsuru.paas.alphagov.co.uk"
   type = "CNAME"
   ttl = "60"
   records = ["${aws_elb.router.dns_name}"]
 }
 
-/* Public Hosted Zone */
-resource "aws_route53_zone" "public" {
-   name = "tsuru.paas.alphagov.co.uk"
-}
