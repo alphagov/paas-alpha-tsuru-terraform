a76b709e3ef321f0a8fa2eec7edc6fa5dbb317fc
Jim Conner <jconner@equalexperts.com>
2015-04-20 11:28:20 +0100

    Additional manifest for creating a postgres server

---
+++ postgres-server.tf
@@ -0,0 +1,11 @@
+/* Single instance postgres server */
+resource "aws_instance" "postgres" {
+  ami = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.private1.id}"
+  security_groups = ["${aws_security_group.default.id}", "${aws_security_group.postgres.id}"]
+  key_name = "${aws_key_pair.deployer.key_name}"
+  tags = {
+    Name = "tsuru-postgres"
+  }
+}
