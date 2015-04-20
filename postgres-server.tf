ab56f143c08f2545363519bfb08ed4e4471b7b9c
Jim Conner <jconner@equalexperts.com>
2015-04-20 11:33:32 +0100

    Removed second security group for postgres as it was not needed.

--- postgres-server.tf
+++ postgres-server.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "postgres" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.private1.id}"
-  security_groups = ["${aws_security_group.default.id}", "${aws_security_group.postgres.id}"]
+  security_groups = ["${aws_security_group.default.id}"]
   key_name = "${aws_key_pair.deployer.key_name}"
   tags = {
     Name = "tsuru-postgres"
