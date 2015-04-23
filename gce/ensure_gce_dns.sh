8d127347704a565bf974057608df7d46e510bf59
Dan Carley <dan.carley@gmail.com>
2015-04-23 16:07:51 +0100

    Add shell script to wrap GCE DNS
    Terraform doesn't have a resource type for Google's Cloud DNS service. So
    for the moment we need to call out to the `gcloud` command line utility.
    
    However it doesn't itself provide a simple way to ensure that a record is
    set to the value that we want. So we need to wrap it with a shell script
    that deletes the existing record of the same name, regardless of it's
    attributes, if it already exists.

---
+++ gce/ensure_gce_dns.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eu
+
+# $1 GCE zone friendly name (e.g. tsuru2)
+# $2 DNS record name
+# $3 TTL
+# $4 Record Type
+# $5 Destination/Value
+
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 <gce_friendly_zone_name> <dns_record_name> <ttl> <record_type> <value>"
+  exit 2
+fi
+
+gce_friendly_name=$1
+dns_record_name=$2
+ttl=$3
+record_type=$4
+value=$5
+
+# Start a new transaction
+trap "gcloud dns record-sets transaction abort -z $gce_friendly_name" ERR
+gcloud dns record-sets transaction start -z "$gce_friendly_name"
+
+# Firstly let's check if the record already exists
+record_line=$(gcloud dns record-sets --zone "$1" list | awk '$1=="'$dns_record_name'."')
+
+if [ -n "$record_line" ] ; then
+  gcloud dns record-sets transaction remove -z $gce_friendly_name \
+    --name $(echo $record_line | awk {'print $1'}) \
+    --ttl $(echo $record_line | awk {'print $3'}) \
+    --type $(echo $record_line | awk {'print $2'}) \
+    $(echo $record_line | awk {'print $4'})
+fi
+
+gcloud dns record-sets transaction add -z $gce_friendly_name \
+  --name $dns_record_name \
+  --ttl $ttl \
+  --type $record_type \
+  "$value"
+
+# Execute the transaction on GCE
+gcloud dns record-sets transaction execute -z $gce_friendly_name
