26edbdd9aff548ca4b4eb203725efae8200d7bc4
Dan Carley <dan.carley@gmail.com>
2015-04-23 14:21:39 +0100

    Copy app-servers for GCE
    Contrary to the name this contains instances and load balancers for:
    
    - API
    - Router
    - Gandalf
    
    We should split these out for both at a later date.
    
    Not using auto scaling groups because they aren't supported by Terraform for
    GCE. We're currently unsure about whether we're going to continue using auto
    scaling groups for AWS because we haven't integrated them with Ansible's
    config management.
    
    Any tags necessary for firewall rules will be fixed in a following commit.

---
+++ gce/app-servers.tf
@@ -0,0 +1,89 @@
+/* API and Docker servers */
+resource "google_compute_instance" "tsuru-app" {
+  count = 2
+  name = "tsuru-app-${count.index}"
+  machine_type = "n1-standard-1"
+  zone = "${var.gce_zone}"
+  disk {
+    image = "${var.os_image}"
+  }
+  network_interface {
+    network = "${google_compute_network.network1.name}"
+  }
+  metadata {
+    sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
+  }
+  service_account {
+    scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
+  }
+  depends_on = [ "google_compute_network.network1" ]
+  tags = [ "private" ]
+}
+
+/* Gandalf server */
+resource "google_compute_instance" "gandalf" {
+  name = "tsuru-app-gandalf"
+  machine_type = "n1-standard-1"
+  zone = "${var.gce_zone}"
+  disk {
+    image = "${var.os_image}"
+  }
+  network_interface {
+    network = "${google_compute_network.network1.name}"
+    access_config {}
+  }
+  metadata {
+    sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
+  }
+  service_account {
+    scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
+  }
+  depends_on = [ "google_compute_network.network1" ]
+  tags = [ "public" ]
+}
+
+/* API load balancer */
+resource "google_compute_target_pool" "api" {
+  name = "tsuru-api-lb"
+  instances = [ "${google_compute_instance.tsuru-app.*.self_link}" ]
+  health_checks = [ "${google_compute_http_health_check.http-check.name}" ]
+}
+resource "google_compute_forwarding_rule" "api" {
+  name = "tsuru-api-lb"
+  target = "${google_compute_target_pool.api.self_link}"
+  port_range = 8080
+}
+
+/* Routers */
+resource "google_compute_instance" "router" {
+  count = 2
+  name = "tsuru-router-${count.index}"
+  machine_type = "n1-standard-1"
+  zone = "${var.gce_zone}"
+  disk {
+    image = "${var.os_image}"
+  }
+  network_interface {
+    network = "${google_compute_network.network1.name}"
+  }
+  metadata {
+    sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
+  }
+  service_account {
+    scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
+  }
+  depends_on = [ "google_compute_network.network1" ]
+  tags = [ "private" ]
+}
+
+/* API load balancer */
+resource "google_compute_target_pool" "router" {
+  name = "tsuru-router-lb"
+  instances = [ "${google_compute_instance.router.*.self_link}" ]
+  health_checks = [ "${google_compute_http_health_check.http-check.name}" ]
+}
+resource "google_compute_forwarding_rule" "router" {
+  name = "tsuru-router-lb"
+  target = "${google_compute_target_pool.router.self_link}"
+  port_range = 80
+}
