ee9698c61706f4141d93c9808a6b59f614eb11ab
Dan Carley <dan.carley@gmail.com>
2015-04-23 14:03:57 +0100

    Add GCE firewall rules for Gandalf and web
    Matching AWS. The instances with these tags will be created in a follow
    commit.

--- gce/security-groups.tf
+++ gce/security-groups.tf
@@ -12,3 +12,33 @@ resource "google_compute_firewall" "nat" {
     ports = [ 22 ]
   }
 }
+
+/* Security group for the Gandalf server */
+resource "google_compute_firewall" "gandalf" {
+  name = "tsuru-gandalf"
+  description = "Security group for Gandalf instance that allows SSH access from internet"
+  network = "${google_compute_network.network1.name}"
+
+  source_ranges = [ "80.194.77.90/32", "80.194.77.100/32" ]
+  target_tags = [ "gandalf" ]
+
+  allow {
+    protocol = "tcp"
+    ports = [ 22 ]
+  }
+}
+
+/* Security group for the web */
+resource "google_compute_firewall" "web" {
+  name = "web-tsuru"
+  description = "Security group for web that allows web traffic from internet"
+  network = "${google_compute_network.network1.name}"
+
+  source_ranges = [ "80.194.77.90/32", "80.194.77.100/32" ]
+  target_tags = [ "web" ]
+
+  allow {
+    protocol = "tcp"
+    ports = [ 80, 8080, 443 ]
+  }
+}
