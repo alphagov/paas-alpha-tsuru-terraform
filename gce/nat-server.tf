0230f1fcee0b50dcf6978bfd8d34faafe1721c5e
Dan Carley <dan.carley@gmail.com>
2015-04-23 11:17:50 +0100

    Replace GCE jumpbox with AWS-like NAT
    In order to match the AWS configuration. Also applying the provisioner which
    configures this machine to do forwarding.
    
    The `can_ip_forward` option on GCE is equivalent to `source_dest_check` on
    AWS.

---
+++ gce/nat-server.tf
@@ -0,0 +1,39 @@
+/* Jumpbox Server Instance */
+resource "google_compute_instance" "nat" {
+    name = "tsuru-nat"
+    machine_type = "n1-standard-1"
+    zone = "${var.gce_zone}"
+    disk {
+        image = "${var.os_image}"
+    }
+    network_interface {
+        network = "${google_compute_network.network1.name}"
+        access_config {
+        }
+    }
+    metadata {
+        sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
+    }
+    service_account {
+        scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
+    }
+    depends_on = [ "google_compute_network.network1" ]
+
+    can_ip_forward = true
+    connection {
+        user = "${var.user}"
+        key_file = "ssh/insecure-deployer"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "yum update -y aws*",
+            "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
+            "echo 0 | sudo tee /proc/sys/net/ipv4/conf/eth0/send_redirects",
+            "sudo iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE",
+            "sudo iptables-save | sudo tee /etc/sysconfig/iptables",
+            "sudo mkdir -p /etc/sysctl.d/",
+            "echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/nat.conf",
+            "echo 'net.ipv4.conf.eth0.send_redirects = 0' | sudo tee -a /etc/sysctl.d/nat.conf"
+        ]
+    }
+}
