3d5d3556cdb469c291b55cf2e5e92b207c4384cf
Jim Conner <jim@secret.org.uk>
2015-04-24 11:15:25 +0100

    Added timstamp+uuid transaction-file name to prevent request clash
    We noticed a problem when terraform was attempting to change two
    records in parallel, whereby the gcloud command was trying to use
    the transaction.yaml file for both operations simultaneously.
    The gcloud command can accept a --transaction-file flag, so by
    using unique transaction file names we prevent the clash.

--- gce/ensure_gce_dns.sh
+++ gce/ensure_gce_dns.sh
@@ -18,26 +18,37 @@ ttl=$3
 record_type=$4
 value=$5
 
+# Generate a unique transaction file name
+# milliseconds-since-epoch with random uuid suffix
+transaction_file=`date +%s%3N-``uuidgen`
+
 # Start a new transaction
-trap "gcloud dns record-sets transaction abort -z $gce_friendly_name" ERR
-gcloud dns record-sets transaction start -z "$gce_friendly_name"
+trap "gcloud dns record-sets transaction abort --transaction-file $transaction_file -z $gce_friendly_name" ERR
+gcloud dns record-sets transaction start --transaction-file $transaction_file -z "$gce_friendly_name"
 
 # Firstly let's check if the record already exists
 record_line=$(gcloud dns record-sets --zone "$1" list | awk '$1=="'$dns_record_name'."')
 
 if [ -n "$record_line" ] ; then
-  gcloud dns record-sets transaction remove -z $gce_friendly_name \
+  echo "Need to first remove record: ${dns_record_name}"
+  gcloud dns record-sets transaction remove \
+    --transaction-file $transaction_file \
+    -z $gce_friendly_name \
     --name $(echo $record_line | awk {'print $1'}) \
     --ttl $(echo $record_line | awk {'print $3'}) \
     --type $(echo $record_line | awk {'print $2'}) \
     $(echo $record_line | awk {'print $4'})
 fi
 
-gcloud dns record-sets transaction add -z $gce_friendly_name \
+gcloud dns record-sets transaction add \
+  --transaction-file $transaction_file \
+  -z $gce_friendly_name \
   --name $dns_record_name \
   --ttl $ttl \
   --type $record_type \
   "$value"
 
 # Execute the transaction on GCE
-gcloud dns record-sets transaction execute -z $gce_friendly_name
+gcloud dns record-sets transaction execute \
+  --transaction-file $transaction_file \
+  -z $gce_friendly_name
