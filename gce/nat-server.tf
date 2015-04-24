dfa55b44d619bdff11dc93021fffe464bc951327
Dan Carley <dan.carley@gmail.com>
2015-04-24 09:01:08 +0100

    Standardise on 2sp idents for all GCE configs
    This matches *most* of the configs I've been writing (because it's how my
    editor is configured) and matches the AWS configs.
    
    This commit is best viewed with git's `-b` option or GitHub's `?w=1` option
    - which should show no changes at all.

--- gce/nat-server.tf
+++ gce/nat-server.tf
@@ -1,45 +1,45 @@
 /* Jumpbox Server Instance */
 resource "google_compute_instance" "nat" {
-    name = "tsuru-nat"
-    machine_type = "n1-standard-1"
-    zone = "${element(split(",", var.gce_zones), count.index)}"
-    disk {
-        image = "${var.os_image}"
+  name = "tsuru-nat"
+  machine_type = "n1-standard-1"
+  zone = "${element(split(",", var.gce_zones), count.index)}"
+  disk {
+    image = "${var.os_image}"
+  }
+  network_interface {
+    network = "${google_compute_network.network1.name}"
+    access_config {
     }
-    network_interface {
-        network = "${google_compute_network.network1.name}"
-        access_config {
-        }
-    }
-    metadata {
-        sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
-    }
-    service_account {
-        scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
-    }
-    depends_on = [ "google_compute_network.network1" ]
+  }
+  metadata {
+    sshKeys = "${var.user}:${file(\"${var.ssh_key_path}")}"
+  }
+  service_account {
+    scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
+  }
+  depends_on = [ "google_compute_network.network1" ]
 
-    can_ip_forward = true
-    connection {
-        user = "${var.user}"
-        key_file = "ssh/insecure-deployer"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
-            "echo 0 | sudo tee /proc/sys/net/ipv4/conf/eth0/send_redirects",
-            "sudo apt-get update -qq",
-            "sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq iptables-persistent",
-            "sudo iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE",
-            "sudo service iptables-persistent save",
-            "sudo mkdir -p /etc/sysctl.d/",
-            "echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/nat.conf",
-            "echo 'net.ipv4.conf.eth0.send_redirects = 0' | sudo tee -a /etc/sysctl.d/nat.conf"
-        ]
-    }
-    tags = [ "public", "nat" ]
+  can_ip_forward = true
+  connection {
+    user = "${var.user}"
+    key_file = "ssh/insecure-deployer"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
+      "echo 0 | sudo tee /proc/sys/net/ipv4/conf/eth0/send_redirects",
+      "sudo apt-get update -qq",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq iptables-persistent",
+      "sudo iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE",
+      "sudo service iptables-persistent save",
+      "sudo mkdir -p /etc/sysctl.d/",
+      "echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/nat.conf",
+      "echo 'net.ipv4.conf.eth0.send_redirects = 0' | sudo tee -a /etc/sysctl.d/nat.conf"
+    ]
+  }
+  tags = [ "public", "nat" ]
 
-    provisioner "local-exec" {
-      command = "./ensure_gce_dns.sh ${var.dns_zone_name} nat.tsuru2.paas.alphagov.co.uk 60 A ${google_compute_instance.nat.network_interface.0.access_config.0.nat_ip}"
-    }
+  provisioner "local-exec" {
+    command = "./ensure_gce_dns.sh ${var.dns_zone_name} nat.tsuru2.paas.alphagov.co.uk 60 A ${google_compute_instance.nat.network_interface.0.access_config.0.nat_ip}"
+  }
 }
