daebae8a7c6363b2d1428026e6bff8cd487a7d9c
Dan Carley <dan.carley@gmail.com>
2015-04-27 11:12:39 +0100

    Add outputs for registry/postgres/sslproxy/router
    So that we know the private IP addresses to add to Ansible's inventory file.
    I've updated both GCE and AWS so that they have parity.
    
    Except for the sslproxy instances on AWS, because Terraform doesn't know
    about the instances which have been created by the auto-scaling group.

--- gce/outputs.tf
+++ gce/outputs.tf
@@ -6,6 +6,10 @@ output "app.1.ip" {
   value = "${google_compute_instance.tsuru-app.1.network_interface.0.address}"
 }
 
+output "docker-registry.private_ip" {
+  value = "${google_compute_instance.docker-registry.network_interface.0.address}"
+}
+
 output "gandalf.private.ip" {
   value = "${google_compute_instance.gandalf.network_interface.0.address}"
 }
@@ -16,6 +20,18 @@ output "gandalf.public.ip" {
 
 output "nat.ip" {
   value = "${google_compute_instance.nat.network_interface.0.access_config.0.nat_ip}"
+}
+
+output "postgres.private_ip" {
+  value = "${google_compute_instance.postgres.network_interface.0.address}"
+}
+
+output "sslproxy.*.private_ip" {
+  value = "${join(",", google_compute_instance.tsuru-sslproxy.*.network_interface.0.address)}"
+}
+
+output "router.*.private_ip" {
+  value = "${join(",", google_compute_instance.router.*.network_interface.0.address)}"
 }
 
 output "elb.hostname" {
