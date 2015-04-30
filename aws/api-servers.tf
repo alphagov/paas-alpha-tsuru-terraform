27fbccbe16998f00cb9670c156c40868ae9f842b
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-30 10:10:12 +0100

    Avoid using cloud-clonfig for Autoscaling instances
    To be consistent with GCE, which is not using cloud-config.
    The commands ran in user-data are being executed by the Ansible provisioner anyway.
    See also https://github.com/alphagov/tsuru-ansible/pull/27
    
    Next phase will be to remove cloud-config/user-data from single instances (Gandalf, Docker) as well, but that requires resource replacement, so we need to be plan accordingly.

--- aws/api-servers.tf
+++ aws/api-servers.tf
@@ -4,7 +4,6 @@ resource "aws_launch_configuration" "api" {
   instance_type = "t2.medium"
   security_groups = ["${aws_security_group.default.id}"]
   key_name = "${aws_key_pair.deployer.key_name}"
-  user_data = "${file(\"cloud-config/app.yml\")}"
   lifecycle {
     create_before_destroy = true
   }
