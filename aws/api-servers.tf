340f50c48b62c2ece24ac01fe794fc4254feff54
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2015-04-30 15:06:17 +0100

    Workaround for cycle dependencies in Terraform
    We were getting some cycle dependencies error when applying the config without existing .tfstate.
    We double checked and we have no cycles at all.
    For reference, see:
      * [What is the dependency cycle in this? #1475](https://github.com/hashicorp/terraform/issues/1475)
      * [Dependency cycle errors with modules #1637](https://github.com/hashicorp/terraform/issues/1637)

--- aws/api-servers.tf
+++ aws/api-servers.tf
@@ -4,9 +4,11 @@ resource "aws_launch_configuration" "api" {
   instance_type = "t2.medium"
   security_groups = ["${aws_security_group.default.id}"]
   key_name = "${var.key_pair_name}"
+/*
   lifecycle {
     create_before_destroy = true
   }
+*/
 }
 
 /* API servers Autoscaling Group */
