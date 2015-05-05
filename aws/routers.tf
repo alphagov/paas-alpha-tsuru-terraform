ca6f9aeea4a7589c69d0526f79ca690ef8d32803
Dan Carley <dan.carley@gmail.com>
2015-05-05 18:12:35 +0100

    aws: Split instances across availability zones
    These instances were previously in auto-scaling groups and would have been
    distributed across the two availability zones that we have been using (a and
    b). This commits restores that by looking up the appropriate zone and subnet
    based on the `count`. The `element()` function will wrap so a third instance
    will be assigned to the first zone and so on.

--- aws/routers.tf
+++ aws/routers.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "router" {
   count = 2
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.medium"
-  subnet_id = "${aws_subnet.private.0.id}"
+  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+  availability_zone = "${element(aws_subnet.private.*.availability_zone, count.index)}"
   security_groups = ["${aws_security_group.default.id}"]
   key_name = "${var.key_pair_name}"
   tags = {
