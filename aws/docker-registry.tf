637c01420fdabb6555e9bfe27f4c43e9a2520b2c
Dan Carley <dan.carley@gmail.com>
2015-06-22 15:05:47 +0100

    aws: Comment out IAM role resources
    Per the comment:
    
    > These resources are commented because not all of us have access to create
    > and modify things in IAM. This has been run once by dcarley and remain
    > here for reference or future modification.
    
    Using `#` comments instead of `/* */` because hashicorp/terraform#2248
    results in the following error:
    
        ➜  aws git:(docker_registry_s3_role) ✗ terraform apply -var env=dcarley
        Error configuring: 2 error(s) occurred:
    
        * aws_route53_record.docker-registry: missing dependency: aws_instance.docker-registry
        * output.docker-registry.private_ip: missing dependency: aws_instance.docker-registry

--- aws/docker-registry.tf
+++ aws/docker-registry.tf
@@ -1,45 +1,50 @@
-resource "aws_iam_role" "docker-registry" {
-  name = "${var.registry_s3_rolename}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": { "Service": "ec2.amazonaws.com" },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "docker-registry" {
-  name   = "${var.registry_s3_rolename}"
-  role   = "${aws_iam_role.docker-registry.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Action": [ "s3:*" ],
-      "Resource": [
-        "arn:aws:s3:::*-${var.registry_s3_bucketname}",
-        "arn:aws:s3:::*-${var.registry_s3_bucketname}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "docker-registry" {
-  name  = "${var.registry_s3_rolename}"
-  roles = [ "${aws_iam_role.docker-registry.id}" ]
-}
+#
+# These resources are commented because not all of us have access to create
+# and modify things in IAM. This has been run once by dcarley and remains
+# here for reference or future modification.
+#
+# resource "aws_iam_role" "docker-registry" {
+#   name = "${var.registry_s3_rolename}"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "",
+#       "Effect": "Allow",
+#       "Principal": { "Service": "ec2.amazonaws.com" },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
+#
+# resource "aws_iam_role_policy" "docker-registry" {
+#   name   = "${var.registry_s3_rolename}"
+#   role   = "${aws_iam_role.docker-registry.id}"
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "",
+#       "Effect": "Allow",
+#       "Action": [ "s3:*" ],
+#       "Resource": [
+#         "arn:aws:s3:::*-${var.registry_s3_bucketname}",
+#         "arn:aws:s3:::*-${var.registry_s3_bucketname}/*"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
+#
+# resource "aws_iam_instance_profile" "docker-registry" {
+#   name  = "${var.registry_s3_rolename}"
+#   roles = [ "${aws_iam_role.docker-registry.id}" ]
+# }
 
 resource "aws_instance" "docker-registry" {
   ami = "${lookup(var.amis, var.region)}"
