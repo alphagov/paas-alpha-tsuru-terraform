2882d56a60e08b6fe558d3337533059aa0ac782d
Russ Garrett <russ@garrett.co.uk>
2015-07-02 15:20:20 +0100

    Comment out resources needing privileged IAM role

--- aws/postgres-server.tf
+++ aws/postgres-server.tf
@@ -1,45 +1,49 @@
-resource "aws_iam_role" "postgres" {
-  name = "${var.postgres_s3_rolename}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": { "Service": "ec2.amazonaws.com" },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "postgres" {
-  name   = "${var.postgres_s3_rolename}"
-  role   = "${aws_iam_role.postgres.id}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Action": [ "s3:*" ],
-      "Resource": [
-        "arn:aws:s3:::*-${var.postgres_s3_bucketname}",
-        "arn:aws:s3:::*-${var.postgres_s3_bucketname}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "postgres" {
-  name  = "${var.postgres_s3_rolename}"
-  roles = [ "${aws_iam_role.postgres.id}" ]
-}
+#
+# These resources are commented out because not everyone has permissions
+# necessary to create them.
+#
+#resource "aws_iam_role" "postgres" {
+#  name = "${var.postgres_s3_rolename}"
+#  assume_role_policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Sid": "",
+#      "Effect": "Allow",
+#      "Principal": { "Service": "ec2.amazonaws.com" },
+#      "Action": "sts:AssumeRole"
+#    }
+#  ]
+#}
+#EOF
+#}
+#
+#resource "aws_iam_role_policy" "postgres" {
+#  name   = "${var.postgres_s3_rolename}"
+#  role   = "${aws_iam_role.postgres.id}"
+#  policy = <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [
+#    {
+#      "Sid": "",
+#      "Effect": "Allow",
+#      "Action": [ "s3:*" ],
+#      "Resource": [
+#        "arn:aws:s3:::*-${var.postgres_s3_bucketname}",
+#        "arn:aws:s3:::*-${var.postgres_s3_bucketname}/*"
+#      ]
+#    }
+#  ]
+#}
+#EOF
+#}
+#
+#resource "aws_iam_instance_profile" "postgres" {
+#  name  = "${var.postgres_s3_rolename}"
+#  roles = [ "${aws_iam_role.postgres.id}" ]
+#}
 
 resource "aws_instance" "postgres" {
   ami = "${lookup(var.amis, var.region)}"
