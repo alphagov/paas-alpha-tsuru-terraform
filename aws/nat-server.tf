70e15dcaa4072fe52ed27e9184fb6c52ebd9f5c8
Michal Tekel <mtekel@gmail.com>
2015-07-08 17:19:57 +0100

    Make AWS NAT server run on ubuntu
    Make the AWS NAT server run on ubuntu, just like the GCE NAT and all other hosts.

--- aws/nat-server.tf
+++ aws/nat-server.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nat" {
-  ami = "${lookup(var.nat_ami, var.region)}"
+  ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.public.0.id}"
   security_groups = ["${aws_security_group.default.id}", "${aws_security_group.nat.id}"]
@@ -9,16 +9,17 @@ resource "aws_instance" "nat" {
     Name = "${var.env}-tsuru-nat"
   }
   connection {
-    user = "ec2-user"
+    user = "ubuntu"
     key_file = "ssh/insecure-deployer"
   }
   provisioner "remote-exec" {
     inline = [
-      "yum update -y aws*",
       "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
       "echo 0 | sudo tee /proc/sys/net/ipv4/conf/eth0/send_redirects",
+      "sudo apt-get update -qq",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq iptables-persistent",
       "sudo iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE",
-      "sudo iptables-save | sudo tee /etc/sysconfig/iptables",
+      "sudo service iptables-persistent save",
       "sudo mkdir -p /etc/sysctl.d/",
       "echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/nat.conf",
       "echo 'net.ipv4.conf.eth0.send_redirects = 0' | sudo tee -a /etc/sysctl.d/nat.conf"
