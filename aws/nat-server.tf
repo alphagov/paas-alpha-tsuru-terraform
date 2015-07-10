13e3fb61d1f7e09df4d24accc3d3fe3aae1fda1d
Hector Rivas Gandara <keymon@gmail.com>
2015-07-10 11:17:09 +0100

    Change the script to setup NAT routing servers
    There were a pair of issues in the configuration of NAT routing. First,
    the inline commands in terraform will not stop the execution if they
    fail, you have to use an external script for that.
    
    Second, while installing a package at boot time with the provisioner,
    cloud-init might be running and it changes the upstream sources.list.
    Because that we add logic to wait for cloud-init to finish.

--- aws/nat-server.tf
+++ aws/nat-server.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "nat" {
   security_groups = ["${aws_security_group.default.id}", "${aws_security_group.nat.id}"]
   key_name = "${var.key_pair_name}"
   source_dest_check = false
-  tags = { 
+  tags = {
     Name = "${var.env}-tsuru-nat"
   }
   connection {
@@ -13,16 +13,6 @@ resource "aws_instance" "nat" {
     key_file = "ssh/insecure-deployer"
   }
   provisioner "remote-exec" {
-    inline = [
-      "echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward",
-      "echo 0 | sudo tee /proc/sys/net/ipv4/conf/eth0/send_redirects",
-      "sudo apt-get update -qq",
-      "sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq iptables-persistent",
-      "sudo iptables -t nat -A POSTROUTING -o eth0 -s 0.0.0.0/0 -j MASQUERADE",
-      "sudo service iptables-persistent save",
-      "sudo mkdir -p /etc/sysctl.d/",
-      "echo 'net.ipv4.ip_forward = 1' | sudo tee /etc/sysctl.d/nat.conf",
-      "echo 'net.ipv4.conf.eth0.send_redirects = 0' | sudo tee -a /etc/sysctl.d/nat.conf"
-    ]
+    script = "../setup-nat-routing.sh"
   }
 }
