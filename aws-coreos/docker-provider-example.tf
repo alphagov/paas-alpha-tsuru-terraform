4697ea6d65b9bc9495461376959d9ded37e7f417
Hector Rivas Gandara <keymon@gmail.com>
2015-08-18 11:20:19 +0100

    Comment out the terraform docker configuration
    It is quite risky and problematic, as terraform needs the docker
    connection to do any operation. This means a lots of issues if the
    docker node is unavailable due any reason.

--- aws-coreos/docker-provider-example.tf
+++ aws-coreos/docker-provider-example.tf
@@ -1,46 +1,43 @@
-                                      #
-# This does not work. Providers cannot have a `count`
-# provider "docker" {
-#   count = "${var.coreos_admin_count}"
-#   host = "tcp://${element(aws_instance.coreos-admin.*.private_ip, count.index)}:4243/"
-#   alias = "admin_${count.index}"
-# }
-
+#                                      #
+## This does not work. Providers cannot have a `count`
+## provider "docker" {
+##   count = "§\\
 #
-# Each host with a docker node needs an adhoc provider :(
+##
+## Each host with a docker node needs an adhoc provider :(
+##
+#provider "docker" {
+#  host = "tcp://${aws_instance.coreos-admin.0.private_ip}:4243/"
+#  alias = "admin_0"
+#}
+#provider "docker" {
+#  host = "tcp://${aws_instance.coreos-admin.1.private_ip}:4243/"
+#  alias = "admin_1"
+#}
+#provider "docker" {
+#  host = "tcp://${aws_instance.coreos-admin.2.private_ip}:4243/"
+#  alias = "admin_2"
+#}
 #
-provider "docker" {
-  host = "tcp://${aws_instance.coreos-admin.0.private_ip}:4243/"
-  alias = "admin_0"
-}
-provider "docker" {
-  host = "tcp://${aws_instance.coreos-admin.1.private_ip}:4243/"
-  alias = "admin_1"
-}
-provider "docker" {
-  host = "tcp://${aws_instance.coreos-admin.2.private_ip}:4243/"
-  alias = "admin_2"
-}
-
+##
+## This does not work. It does not do interpolation in the provider variable
+##
+## resource "docker_image" "ubuntu_count" {
+##  count = 3
+##  name = "ubuntu:raring"
+##  provider = "docker.admin_${count.index}"
+## }
+##
 #
-# This does not work. It does not do interpolation in the provider variable
 #
-# resource "docker_image" "ubuntu_count" {
-#  count = 3
+#resource "docker_image" "ubuntu" {
 #  name = "ubuntu:raring"
-#  provider = "docker.admin_${count.index}"
-# }
+#  provider = "docker.admin_0"
+#}
+#resource "docker_container" "foo" {
+#  image = "${docker_image.ubuntu.latest}"
+#  name = "foo"
+#  command = [ "/bin/sleep", "6000" ]
+#  provider = "docker.admin_0"
+#}
 #
-
-
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:raring"
-  provider = "docker.admin_0"
-}
-resource "docker_container" "foo" {
-  image = "${docker_image.ubuntu.latest}"
-  name = "foo"
-  command = [ "/bin/sleep", "6000" ]
-  provider = "docker.admin_0"
-}
-
