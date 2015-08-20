049f062718bbf2fd22eb3a15facc5645cbbeef70
Hector Rivas Gandara <keymon@gmail.com>
2015-08-20 10:25:35 +0100

    Format code in gandalf.tf for consistency

--- aws/gandalf.tf
+++ aws/gandalf.tf
@@ -3,7 +3,10 @@ resource "aws_instance" "gandalf" {
   instance_type = "t2.medium"
   subnet_id = "${aws_subnet.public.0.id}"
   associate_public_ip_address = true
-  security_groups = ["${aws_security_group.default.id}", "${aws_security_group.gandalf.id}"]
+  security_groups = [
+    "${aws_security_group.default.id}",
+    "${aws_security_group.gandalf.id}"
+  ]
   key_name = "${var.key_pair_name}"
   tags = {
     Name = "${var.env}-tsuru-gandalf"
