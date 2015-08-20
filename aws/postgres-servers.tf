7a166386875ceb4b3012a662ed73c150c806347d
Hector Rivas Gandara <keymon@gmail.com>
2015-08-20 10:16:23 +0100

    Add dedicated security group for postgres
    Allow access to postgres DB port 5432 only from docker nodes.

--- aws/postgres-servers.tf
+++ aws/postgres-servers.tf
@@ -51,7 +51,10 @@ resource "aws_instance" "postgres" {
   instance_type = "t2.micro"
   subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
   availability_zone = "${element(aws_subnet.private.*.availability_zone, count.index)}"
-  security_groups = ["${aws_security_group.default.id}"]
+  security_groups = [
+    "${aws_security_group.default.id}",
+    "${aws_security_group.postgres.id}"
+  ]
   iam_instance_profile = "${var.postgres_s3_rolename}"
   key_name = "${var.key_pair_name}"
   tags = {
@@ -65,3 +68,21 @@ resource "aws_s3_bucket" "postgres-s3" {
     force_destroy = "${var.force_destroy}"
 }
 
+resource "aws_security_group" "postgres" {
+  name = "${var.env}-postgres"
+  description = "Postgres security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 0
+    to_port   = 5432
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.docker_node.id}"
+    ]
+  }
+
+  tags = {
+    Name = "${var.env}-tsuru-postgres-${count.index}"
+  }
+}
