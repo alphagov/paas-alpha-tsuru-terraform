fdac9aa71a3c417728e5ae293c0e44350625f79e
Hector Rivas Gandara <keymon@gmail.com>
2015-08-20 10:12:59 +0100

    Add dedicated security group for elastic search

--- aws/elasticsearch.tf
+++ aws/elasticsearch.tf
@@ -2,8 +2,30 @@ resource "aws_instance" "elasticsearch" {
   ami = "${lookup(var.ubuntu_amis, var.region)}"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.private.0.id}"
-  security_groups = ["${aws_security_group.default.id}"]
+  security_groups = [
+    "${aws_security_group.default.id}",
+    "${aws_security_group.elasticsearch.id}"
+  ]
   key_name = "${var.key_pair_name}"
+  tags = {
+    Name = "${var.env}-tsuru-elasticsearch"
+  }
+}
+
+resource "aws_security_group" "elasticsearch" {
+  name = "${var.env}-elasticsearch"
+  description = "Elasticsearch security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 0
+    to_port   = 9200
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.docker_node.id}"
+    ]
+  }
+
   tags = {
     Name = "${var.env}-tsuru-elasticsearch"
   }
