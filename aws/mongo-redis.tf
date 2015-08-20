eaa1dd5b2c8c8b00503d5c80312fb2fe12b66051
Hector Rivas Gandara <keymon@gmail.com>
2015-08-20 10:03:15 +0100

    Add dedicated security group for redis db
    Allow traffic to redis port 6379 from tsuru api and routers.
    Redis is use to define routes hipache, which are populated by tsuru api and read by the router.

--- aws/mongo-redis.tf
+++ aws/mongo-redis.tf
@@ -2,10 +2,29 @@ resource "aws_instance" "tsuru-db" {
   ami = "${lookup(var.ubuntu_amis, var.region)}"
   instance_type = "t2.medium"
   subnet_id = "${aws_subnet.private.0.id}"
-  security_groups = ["${aws_security_group.default.id}"]
+  security_groups = [
+    "${aws_security_group.default.id}",
+    "${aws_security_group.redis.id}"
+  ]
   key_name = "${var.key_pair_name}"
   tags = {
     Name = "${var.env}-tsuru-db"
   }
 }
 
+
+resource "aws_security_group" "redis" {
+  name = "redis"
+  description = "Redis security group"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+      from_port = 0
+      to_port = 6379
+      protocol = "tcp"
+      security_groups = [
+        "${aws_security_group.tsuru_api.id}",
+        "${aws_security_group.router.id}"
+      ]
+  }
+}
