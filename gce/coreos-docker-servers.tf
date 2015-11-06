4ca76e3f7b7ad06e512b124767244891d9590477
Hector Rivas Gandara <keymon@gmail.com>
2015-11-06 11:45:55 +0000

    Add new telegraf tags to GCE coreos boxes

--- gce/coreos-docker-servers.tf
+++ gce/coreos-docker-servers.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "coreos-docker" {
-  count = "${var.docker_count}"
   depends_on = [ "template_file.etcd_cloud_config" ]
-  name = "${var.env}-tsuru-coreos-docker-${count.index}"
+  count = "${var.docker_count}"
+  name = "${element(template_file.etcd_cloud_config.*.vars.telegraf_tag_instance_name, count.index)}"
   machine_type = "n1-standard-1"
   zone = "${element(split(",", var.gce_zones), count.index)}"
   disk {
@@ -12,7 +12,7 @@ resource "google_compute_instance" "coreos-docker" {
   }
   metadata {
     sshKeys = "core:${file("${var.ssh_key_path}")}"
-    user-data = "${template_file.etcd_cloud_config.rendered}"
+    user_data = "${element(template_file.etcd_cloud_config.*.rendered, count.index)}"
   }
   service_account {
     scopes = [ "compute-ro", "storage-ro", "userinfo-email" ]
@@ -22,10 +22,14 @@ resource "google_compute_instance" "coreos-docker" {
 
 resource "template_file" "etcd_cloud_config" {
   depends_on = [ "template_file.etcd_discovery_url" ]
+  count = "${var.docker_count}"
   filename = "../coreos-config.yaml.tpl"
   vars {
     etcd_discovery_url = "${file("ETCD_CLUSTER_ID")}"
     docker_registry_host = "${replace(google_dns_record_set.docker-registry.name, "/\.$/", ":${var.registry_port}")}"
+    influx_db_host = "${google_compute_instance.influx-grafana.network_interface.0.address}"
+    telegraf_tag_instance_name = "${var.env}-tsuru-coreos-docker-${count.index}"
+    telegraf_tag_type = "coreos-docker"
   }
 }
 
